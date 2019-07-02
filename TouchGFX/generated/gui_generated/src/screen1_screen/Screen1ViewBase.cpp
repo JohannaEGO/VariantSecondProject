c40525cb7b3169bc2cf71cf1c659e3d10ffc35e7
Johanna <johanna.funk@egoproducts.com>
2019-07-02 14:42:22 +0200

    changed background color to purple

--- TouchGFX/generated/gui_generated/src/screen1_screen/Screen1ViewBase.cpp
+++ TouchGFX/generated/gui_generated/src/screen1_screen/Screen1ViewBase.cpp
@@ -9,7 +9,7 @@ Screen1ViewBase::Screen1ViewBase() :
     buttonCallback(this, &Screen1ViewBase::buttonCallbackHandler)
 {
     box1.setPosition(0, 0, 800, 480);
-    box1.setColor(touchgfx::Color::getColorFrom24BitRGB(139, 240, 214));
+    box1.setColor(touchgfx::Color::getColorFrom24BitRGB(139, 12, 214));
 
     image.setXY(196, 172);
     image.setBitmap(Bitmap(BITMAP_B5_ID));
